6c7ed799f4be21bde074df51d609e3ede99f22ed
reborafs <reborafs@gmail.com>
2025-11-02 19:52:38 -0300

    controller fix

--- src/main/java/com/sip3/backend/features/payment/controller/PaymentController.java
+++ src/main/java/com/sip3/backend/features/payment/controller/PaymentController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/api/v1/payments")
 public class PaymentController {
 
     private final PaymentService paymentService;
